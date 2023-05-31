787d2bff81971e3a195be05eb63eac6cebf682ef
Yevhenii Molchanov <oocrazyb@gmail.com>
2023-05-31 15:28:06 +0300

    Create mix.exs

---
+++ mix.exs
@@ -0,0 +1,26 @@
+defmodule CredoAction.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :credo_action,
+      version: "0.1.0",
+      elixir: "~> 1.12",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      mod: {CredoAction.Application, []},
+      extra_applications: [:logger]
+    ]
+  end
+
+  defp deps do
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false}
+    ]
+  end
+end
